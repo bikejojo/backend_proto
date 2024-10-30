312fd473443f601152f0ff69a4707627ad667905
MichaelHE-WC <dev3@workcorp.net>
2024-10-30 18:36:20 -0400

    request 1.0

--- app/GraphQL/Queries/RequestQuery.php
+++ app/GraphQL/Queries/RequestQuery.php
@@ -2,6 +2,9 @@
 
 namespace App\GraphQL\Queries;
 
+use App\Models\Tecnico;
+use App\Models\Solicitud;
+
 final readonly class RequestQuery
 {
     /** @param  array{}  $args */
@@ -11,6 +14,25 @@ final readonly class RequestQuery
     }
 
     public function technicalId($root , array $args){
+        $technicalId = $args['id'];
+        $technical = Tecnico::find($technicalId);
+        if(!isset($technical)){
+            return [
+                'message' => 'No existe tecnico'
+            ];
+        }
 
+        $technicaId = $technical->id;
+        $request = Solicitud::where('technicianId',$technicaId)->get();
+        if(!isset($request)){
+            return [
+                'message' => 'No existe solicitudes del tecnico.'
+            ];
+        }
+        return [
+            'message' => 'No existe solicitudes del tecnico.',
+            'request' => $request,
+            'technicial' =>  $technical
+        ];
     }
 }
