82aa33c64dace2115c6845520b02f3790418a766
MichaelHE-WC <dev3@workcorp.net>
2024-10-30 18:28:49 -0400

    complementario a request

---
+++ app/GraphQL/Queries/RequestQuery.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+final readonly class RequestQuery
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        // TODO implement the resolver
+    }
+
+    public function technicalId($root , array $args){
+
+    }
+}
