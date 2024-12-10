403724265d7c8cd806b253336211442dd7259c54
bikejojo <jorgecsri@gmail.com>
2024-12-10 16:30:50 -0400

    payment_V1.2

--- app/GraphQL/Mutations/PagoMutations.php
+++ app/GraphQL/Mutations/PagoMutations.php
@@ -4,6 +4,8 @@ namespace App\GraphQL\Mutations;
 
 use App\Models\Payment;
 use App\Models\Tecnico;
+use App\Models\Suscripcion;
+use App\Models\Pago;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
@@ -19,14 +21,14 @@ class PagoMutations
                 'message' => 'No se encontró la suscripción especificada.'
             ];
         }
-        $technician=Tecnico::find($paymentData['technicianId']);
+        $technician=Tecnico::find($subscription->technicianId);
         if(!$technician){
             return [
                 'message' => 'No se encontró el tecnico.'
             ];
         }
 
-        $comprobacion=Suscripcion::where('id',$paymentData['subscriptionId'])->where('technicianId',$paymentData['technicianId'])->first();
+        $comprobacion=Suscripcion::where('id',$paymentData['subscriptionId'])->where('technicianId',$subscription->technicianId)->first();
         if(!$comprobacion){
             return[
                 'message'=>'No existe relacion entre tecnico y suscripcion.'
@@ -36,6 +38,7 @@ class PagoMutations
         DB::beginTransaction();
         try {
             // Crear un nuevo pago con estado "Pendiente" (status = 0)
+           
             $payment = Pago::create([
                 'bank' => $paymentData['bank'],
                 'account' => $paymentData['account'],
@@ -46,7 +49,7 @@ class PagoMutations
                 'method_payment' => $paymentData['method_payment'],
                 'date_payment' => Carbon::now(),
                 'photo_qr' => $paymentData['photo_qr'] ?? null,
-                'subscription_id' => $subscription->id,
+                'subscriptionId' => $subscription->id,
                 'status' => 0, // Pendiente
             ]);
 
@@ -80,13 +83,13 @@ class PagoMutations
                 'message' => 'No se encontró el pago especificado.'
             ];
         }
-        $subscription = Suscripcion::where('id',$payment->subcriptionId);
+        $subscription = Suscripcion::where('id',$payment->subscriptionId)->first();
         if (!$subscription) {
             return [
                 'message' => 'No se encontró la suscripción especificada.'
             ];
         }
-        $technician=Tecnico::where('id',$subscription->technicianId);
+        $technician=Tecnico::where('id',$subscription->technicianId)->first();
         if(!$technician){
             return [
                 'message' => 'No se encontró el tecnico.'
