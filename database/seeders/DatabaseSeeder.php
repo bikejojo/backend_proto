113f19a9c760b4fd7a724f63b56b5ecd58aeaf9a
MichaelHE-WC <dev3@workcorp.net>
2024-12-19 15:05:03 -0400

    cambios_v4.8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -43,7 +43,10 @@ class DatabaseSeeder extends Seeder
         Ciudad::create(['name'=>'Oruro']);
         Ciudad::create(['name'=>'Potosi']);
 
-
+        Group::create(['name'=>'Hogar y Mantenimiento']);
+        Group::create(['name'=>'Tecnologia']);
+        Group::create(['name'=>'Automotriz']);
+        Group::create(['name'=>'Servicios Especializados']);
         #habilidades para tecnicos
         Habilidad::create(['name' => 'Plomeria']); //1
         Habilidad::create(['name' => 'Electricista']); //1
@@ -63,11 +66,36 @@ class DatabaseSeeder extends Seeder
         Habilidad::create(['name' => 'Tecnico en electrodomesticos']); //2
         Habilidad::create(['name' => 'Limpieza General']); //1
         Habilidad::create(['name' => 'Tecnico en telefonia movil']); //2
+
+/*
+        Habilidad::create(['name' => 'Aires Acondicionados']); //1
+        Habilidad::create(['name' => 'Calefonos']); //1
+        Habilidad::create(['name' => 'Electricistas']); //1
+        Habilidad::create(['name' => 'Plomero']); //1
+        Habilidad::create(['name' => 'Piscinero / Jardineria']); //3
+        Habilidad::create(['name' => 'Veterinario']); //3
+        Habilidad::create(['name' => 'Pintores']); //4
+        Habilidad::create(['name' => 'Cocinas / Helederas / Lavadoras / Hornos']); //1
+        Habilidad::create(['name' => 'Zapatero']);
+        Habilidad::create(['name' => 'Lava autos']); //4
+        Habilidad::create(['name' => 'Cambios de aceites / Recojo de vehiculos']); //4
+        Habilidad::create(['name' => 'Baterias']); //4
+        Habilidad::create(['name' => 'Servicio de computacion']); //4
+        Habilidad::create(['name' => 'Copia de llaves']); //2
+        Habilidad::create(['name' => 'Servicio de grua']); //2
+        Habilidad::create(['name' => 'Catering / Churrasquero']); //2
+        Habilidad::create(['name' => 'Garzones']); //1
+        Habilidad::create(['name' => 'Alquiler sillas, Mesas , Vajillas']); //2
+        Habilidad::create(['name' => 'Barra de bebidas / Licoreria']); //2
+        Habilidad::create(['name' => 'Grupo musica, mariachi, bandas']); //2
+
         #
-        Group::create(['name'=>'Hogar y Mantenimiento']);
-        Group::create(['name'=>'Tecnologia']);
-        Group::create(['name'=>'Automotriz']);
-        Group::create(['name'=>'Servicios Especializados']);
+
+/*
+        Group::create(['name'=>'Servicios Técnicos y de Mantenimiento']);
+        Group::create(['name'=>'Servicios para el Hogar y Llaves']);
+        Group::create(['name'=>'Servicios de Transporte y Automotriz']);
+        Group::create(['name'=>'Servicios de Catering y Eventos']); */
         #grupo de habilidades
         Skills_group::create(['groupId'=>1,'skillsId'=>1]);
         Skills_group::create(['groupId'=>1,'skillsId'=>2]);
@@ -91,7 +119,33 @@ class DatabaseSeeder extends Seeder
         Skills_group::create(['groupId'=>4,'skillsId'=>12]);
         Skills_group::create(['groupId'=>4,'skillsId'=>13]);
 
+/*
+        #grupo de habilidades
+        Skills_group::create(['groupId'=>1,'skillsId'=>1]);
+        Skills_group::create(['groupId'=>1,'skillsId'=>2]);
+        Skills_group::create(['groupId'=>1,'skillsId'=>3]);
+        Skills_group::create(['groupId'=>1,'skillsId'=>4]);
+        Skills_group::create(['groupId'=>1,'skillsId'=>5]);
+        Skills_group::create(['groupId'=>1,'skillsId'=>7]);
+        Skills_group::create(['groupId'=>1,'skillsId'=>8]);
+        Skills_group::create(['groupId'=>1,'skillsId'=>11]);
+        Skills_group::create(['groupId'=>1,'skillsId'=>12]);
+        Skills_group::create(['groupId'=>1,'skillsId'=>13]);
 
+        Skills_group::create(['groupId'=>2,'skillsId'=>6]);
+        Skills_group::create(['groupId'=>2,'skillsId'=>9]);
+        Skills_group::create(['groupId'=>2,'skillsId'=>14]);
+
+        Skills_group::create(['groupId'=>3,'skillsId'=>10]);
+        Skills_group::create(['groupId'=>3,'skillsId'=>15]);
+
+        Skills_group::create(['groupId'=>4,'skillsId'=>16]);
+        Skills_group::create(['groupId'=>4,'skillsId'=>17]);
+        Skills_group::create(['groupId'=>4,'skillsId'=>18]);
+        Skills_group::create(['groupId'=>4,'skillsId'=>19]);
+        Skills_group::create(['groupId'=>4,'skillsId'=>20]);
+
+*/
         #agenda
         Tipo_Actividad::create(['description'=>'mantenimiento','entity_type'=>'service']);
         Tipo_Actividad::create(['description'=>'reparacion','entity_type'=>'service']);
