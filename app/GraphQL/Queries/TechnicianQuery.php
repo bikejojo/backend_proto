aa602d37debe79b93db52258d8a34d8d04d285fd
bikejojo <jorgecsri@gmail.com>
2025-02-05 23:22:29 -0400

    fronted_admin

--- app/GraphQL/Queries/TechnicianQuery.php
+++ app/GraphQL/Queries/TechnicianQuery.php
@@ -4,6 +4,9 @@ namespace App\GraphQL\Queries;
 
 use App\Models\Ciudad;
 use App\Models\Tecnico;
+use App\Models\User;
+use App\Models\Technician_subcripcion;
+use App\Models\Suscripcion;
 use Illuminate\Support\Facades\DB;
 
 class TechnicianQuery
@@ -39,5 +42,27 @@ class TechnicianQuery
             ];
         }
 
+    }
+
+    public function get_technician($root,array $args){
+        try {
+            $technician = Tecnico::join('users','technicians.userId','=','users.id')
+                            ->join('cities','technicians.cityId','=','cities.id')
+                            ->join('technician_subcription','technicians.id','=','technician_subcription.technicianId')
+                            ->join('subcriptions','technician_subcription.subcriptionsId','=','subcriptions.id')
+                            ->select(
+                                'users.ci','cities.name',DB::raw('CONCAT(COALESCE(technicians."firstName", \'\'), \' \', COALESCE(technicians."lastName", \'\')) As full_name'),'technicians.phoneNumber',
+                                'technicians.email','technicians.frontIdCard','technicians.backIdCard','technicians.photo',
+                                'cities.name','subcriptions.name','subcriptions.codeSubcription',
+                                'technician_subcription.status','technician_subcription.starDateSubcription','technician_subcription.endDateSubcription'
+                            )
+                            ->get();
+        } catch (\Exception $e) {
+            return [
+                'message' =>'No existen tecnicos en la ' . $e->getMessage(),
+                'status' => 3,
+                'technicians'  => []
+            ];
+        }
     }
 }
