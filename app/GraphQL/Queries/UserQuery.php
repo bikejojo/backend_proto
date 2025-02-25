cad751e0efbe46a1ee6b56dcbb66bf9fdd4b9a85
MichaelHE-WC <dev3@workcorp.net>
2025-02-25 08:38:09 -0400

    roles

--- app/GraphQL/Queries/UserQuery.php
+++ app/GraphQL/Queries/UserQuery.php
@@ -3,7 +3,15 @@
 namespace App\GraphQL\Queries;
 
 use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 class UserQuery{
+
+    protected $client = 2;
+    protected $technician = 1;
+    protected $admins = 3;
+
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args)
     {
@@ -12,6 +20,46 @@ class UserQuery{
 
     public function usuarioTecnico($root , array $args){
         return User::with('tecnicos')->findOrFail($args['id']);
+    }
+
+    public function getUserRol($root,array $args){
+        try{
+            $userList = User::where('type_user',$this->admins)
+            ->with(['roles', 'permissions'])
+            ->get();
+            //dd($userList);
+
+            if($userList->isEmpty()){
+                return [
+                    'message'=>'No existe datos en la consulta',
+                    'succes'=>'1',
+                    'user'=>[],
+                ];
+            }
+
+            $formattedUsers = $userList->map(function ($user) {
+                return [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'roles' => $user->roles->pluck('name')->filter()->values()->toArray(), // Asegura que no haya valores nulos // Solo nombres de roles
+                    'permissions' => $user->permissions->pluck('name'), // Solo nombres de permisos
+                ];
+            });
+            //dd($formattedUsers);
+            return [
+                'message'=>'listado de usuario',
+                'succes'=>'2',
+                'user'=>$formattedUsers,
+            ];
+
+        } catch (\Exception $e){
+            return [
+                'message'=>'Fallas en la siguiente parte :' . $e->getMessage(),
+                'succes'=>'3',
+                'user'=>null,
+            ];
+        }
     }
 
 }
