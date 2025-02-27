4b97cdbba13c9d0018e100971f52a77dd754e7d4
MichaelHE-WC <dev3@workcorp.net>
2025-02-27 09:41:28 -0400

    fecha-27-feb

--- app/GraphQL/Queries/ServiceQuery.php
+++ app/GraphQL/Queries/ServiceQuery.php
@@ -588,8 +588,8 @@ class ServiceQuery
                                 ->where('requests.clientId', $clientId)
                                 ->where('requests.stateId', 2)
                                 ->whereBetween('services.updatedDateTime', [
-                                    now()->subDay(), // Un día antes
-                                    now()->addDays(5)  // Un día después
+                                    now()->subDays(7), // Un día antes
+                                    now()->addDays(7)  // Un día después
                                 ])
                                 ->select(
                                     'requests.id AS id_requests',
