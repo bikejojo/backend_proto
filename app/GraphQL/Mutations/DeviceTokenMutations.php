841d9ceed9011331b9bfc179d6988d3bf781bb38
MichaelHE-WC <dev3@workcorp.net>
2025-04-17 11:25:36 -0400

    deviceToken

--- app/GraphQL/Mutations/DeviceTokenMutations.php
+++ app/GraphQL/Mutations/DeviceTokenMutations.php
@@ -14,41 +14,73 @@ class DeviceTokenMutations
         DB::beginTransaction();
         try {
             $requestDevice = $args['deviceTokenRequest'];
-            $device = Devices::updateOrCreate([
-                'type_device'=>$requestDevice['type_device'],
-                'name_device'=>$requestDevice['name_device'],
-                'expo_token'=>$requestDevice['expo_token'],
-            ]);
+            if(!Devices::where('expo_token',$requestDevice['expo_token'])->exists()){
+                $device = Devices::updateOrCreate([
+                    'type_device'=>$requestDevice['type_device'],
+                    'name_device'=>$requestDevice['name_device'],
+                    'expo_token'=>$requestDevice['expo_token'],
+                ]);
 
-            $userId = ValidationModels::validation_user($requestDevice['userId']);
-            //dd($userId);
-            if(!$userId){
-                DB::rollBack();
+                $userId = ValidationModels::validation_user($requestDevice['userId']);
+
+                if(!$userId){
+                    $deviceUser = DevicesUser::updateOrCreate([
+                        'device_id'=>$device->id,
+                        'users_id'=> null,
+                    ]);
+                        DB::commit();
+                        return [
+                            'message'=>'Registro exitoso del equipo en el servidor.',
+                            'token'=> $device->expo_token,
+                            'success'=>1,
+                        ];
+                }
+
+                $deviceUser = DevicesUser::updateOrCreate([
+                    'device_id'=>$device->id,
+                    'users_id'=> $userId->first()->id,
+                ]);
+
+                if(!$deviceUser){
+                    DB::rollBack();
+                    return [
+                        'message' => 'Se produjo un error en el guardar dispositivo y usuario.',
+                        'success' => 2
+                    ];
+                }
+                
+                DB::commit();
                 return [
-                    'message' => 'Se produjo un error en el usuario',
-                    'success' => 2
+                    'message'=>'Registro exitoso del equipo en el servidor.',
+                    'token'=> $device->expo_token,
+                    'success'=>1,
                 ];
-            }
-            //dd($userId);
-            $deviceUser = DevicesUser::updateOrCreate([
-                'device_id'=>$device->id,
-                'users_id'=> $userId->first()->id,
-            ]);
+            }else{
+                $deviceExists = Devices::where('expo_token',$requestDevice['expo_token'])->first();
+                $deviceExists->expo_token = $requestDevice['expo_token'];
+                $deviceExists->save();
+                $userId = ValidationModels::validation_user($requestDevice['userId']);
+                if($userId){
+                    $deviceUser = DevicesUser::updateOrCreate([
+                        'device_id'=>$deviceExists->id,
+                        'users_id'=> $userId->first()->id,
+                    ]);
 
-            if(!$deviceUser){
-                DB::rollBack();
-                return [
-                    'message' => 'Se produjo un error en el guardar dispositivo y usuario.',
-                    'success' => 2
-                ];
+                    if(!$deviceUser){
+                        DB::rollBack();
+                        return [
+                            'message' => 'Se produjo un error en el guardar dispositivo y usuario.',
+                            'success' => 2
+                        ];
+                    }
+                    DB::commit();
+                    return [
+                        'message'=>'Registro exitoso del equipo en el servidor.',
+                        'token'=> $deviceExists->expo_token,
+                        'success'=>1,
+                    ];
+                }
             }
-
-            DB::commit();
-            return [
-                'message'=>'Registro exitoso del equipo en el servidor.',
-                'token'=> $device->expo_token,
-                'success'=>1,
-            ];
         } catch (\Exception $e){
             DB::rollBack();
             return [
