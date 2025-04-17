921f4cb12a7530b21c4c05865265ff3d7babb73d
MichaelHE-WC <dev3@workcorp.net>
2025-04-17 18:27:45 -0400

    notifications

--- app/GraphQL/Queries/NotificationsQuery.php
+++ app/GraphQL/Queries/NotificationsQuery.php
@@ -5,6 +5,7 @@ namespace App\GraphQL\Queries;
 use App\Models\Notification;
 use App\Models\NotificationUser;
 use  App\Models\NotificationsDevice;
+use App\Services\ValidationModels;
 use Illuminate\Support\Facades\DB;
 
 class NotificationsQuery
@@ -14,20 +15,59 @@ class NotificationsQuery
     public function getNotificationsTech($root,array $args){
         try {
             $input = $args['Input'];
-            $userId = $input['userId'];
-            $notifications = Notification::where('status', 1)
-                ->where('userId', $userId)
+            $userId = $input['userTech'];
+
+            $user = ValidationModels::validation_user($userId);
+            if(!$user){
+                return [
+                    'message'=>'No se encontró el usuario para el dispositivo especificado.',
+                    'status' => '2'
+                ];
+            }
+            //dd($user);
+            $notifications = Notification::where('type_users', '1')
+                ->where('sender_id', $user->id)
+                ->orderBy('send_at', 'desc')
+                ->get();
+            //dd($notifications);
+            $notificationsUser = NotificationUser::where('user_id', $user->id)
+                ->where('type_users', '1')
                 ->orderBy('created_at', 'desc')
                 ->get();
+            //dd($notificationsUser);
 
-            $notifications->each(function ($notification) {
-                $notification->read = NotificationUser::where('notificationId', $notification->id)->exists();
-            });
-
+        } catch (\Exception $e){
             return [
-                'message' => 'Notificaciones obtenidas correctamente.',
-                'notifications' => $notifications
+                'message'=>'Las siguientes fallas son estasssss: ' - $e->getMessage(),
+                'status' => '3'
             ];
+        }
+    }
+
+    public function getNotificationsClient($root,array $args){
+        try {
+            $input = $args['Input'];
+            $userId = $input['userClient'];
+
+            $user = ValidationModels::validation_user($userId);
+            if(!$user){
+                return [
+                    'message'=>'No se encontró el usuario para el dispositivo especificado.',
+                    'status' => '2'
+                ];
+            }
+            dd($user);
+            $notifications = Notification::where('type_users', '2')
+                ->where('sender_id', $user->id)
+                ->orderBy('send_at', 'desc')
+                ->get();
+            //dd($notifications);
+            $notificationsUser = NotificationUser::where('user_id', $user->id)
+                ->where('type_users', '2')
+                ->orderBy('created_at', 'desc')
+                ->get();
+            //dd($notificationsUser);
+
         } catch (\Exception $e){
             return [
                 'message'=>'Las siguientes fallas son estasssss: ' - $e->getMessage(),
