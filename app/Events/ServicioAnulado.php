01963343cb677f5e1d6bd63482411f1459b4cefe
MichaelHE-WC <dev3@workcorp.net>
2025-04-23 17:20:05 -0400

    event_v2

---
+++ app/Events/ServicioAnulado.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Servicio;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ServicioAnulado
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $service;
+    protected $key;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Servicio $service,$key)
+    {
+        $this->service = $service;
+        $this->key = $key;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
---
+++ app/Events/ServicioCompletado.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Servicio;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ServicioCompletado
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $service;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Servicio $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
