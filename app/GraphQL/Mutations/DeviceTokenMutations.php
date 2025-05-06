63eba1e826cf476951610c5eb4c4d0e152a45e67
MichaelHE-WC <dev3@workcorp.net>
2025-05-06 16:15:53 -0400

    device_2

--- app/GraphQL/Mutations/DeviceTokenMutations.php
+++ app/GraphQL/Mutations/DeviceTokenMutations.php
@@ -12,83 +12,173 @@ use Illuminate\Support\Facades\Log;
 class DeviceTokenMutations
 {
     public function register($root , array $args){
-        Log::info('contenido ' , $args);
-        DB::beginTransaction();
-        try {
-            $requestDevice = $args['deviceTokenRequest'];
-            if(!Devices::where('expo_token',$requestDevice['expo_token'])->exists()){
-                $device = new Devices();
-                    $device->type_device = $requestDevice['type_device'];
-                    $device->name_device = $requestDevice['name_device'];
-                    $device->expo_token = $requestDevice['expo_token'];
-                $device->save();
+        //Log::info('contenido ' , $args);
+        $type = $args["deviceTokenRequest"]["type"];
+        switch($type){
+            case "1":
+                DB::beginTransaction();
+                try {
+                    $requestDevice = $args['deviceTokenRequest'];
+                    if(!Devices::where('expo_token',$requestDevice['expo_token'])->exists()){
+                        $device = new Devices();
+                            $device->type_device = $requestDevice['type_device'];
+                            $device->name_device = $requestDevice['name_device'];
+                            $device->expo_token = $requestDevice['expo_token'];
+                        $device->save();
 
-                $userId = ValidationModels::validation_user($requestDevice['userId']);
+                        //$userId = ValidationModels::validation_user($requestDevice['userId']);
+                        $userTech = ValidationModels::validation_Technician($requestDevice['userId']);
+                        $userId = ValidationModels::validation_user($userTech->userId);
+                        if(!$userId){
+                            $deviceUser =new DevicesUser();
+                                $deviceUser->device_id = $device->id;
+                                $deviceUser->users_id = null;
+                            $deviceUser->save();
 
-                if(!$userId){
-                    $deviceUser =new DevicesUser();
-                        $deviceUser->device_id = $device->id;
-                        $deviceUser->users_id = null;
-                    $deviceUser->save();
+                            DB::commit();
+                            return [
+                                'message'=>'Registro exitoso del equipo invitado en el servidor.',
+                                'token'=> $device->expo_token,
+                                'success'=>1,
+                            ];
+                        }else{
+                            $deviceUser =new DevicesUser();
+                                $deviceUser->device_id = $device->id;
+                                $deviceUser->users_id = $userId->id;
+                            $deviceUser->save();
+                            DB::commit();
+                            return [
+                                'message'=>'Registro exitoso del equipo en el servidor.',
+                                'token'=> $device->expo_token,
+                                'success'=>1,
+                            ];
+                        }
 
-                    DB::commit();
-                    return [
-                        'message'=>'Registro exitoso del equipo en el servidor.',
-                        'token'=> $device->expo_token,
-                        'success'=>1,
-                    ];
-                }else{
-                    $deviceUser =new DevicesUser();
-                        $deviceUser->device_id = $device->id;
-                        $deviceUser->users_id = $userId->id;
-                    $deviceUser->save();
-                    DB::commit();
-                    return [
-                        'message'=>'Registro exitoso del equipo en el servidor.',
-                        'token'=> $device->expo_token,
-                        'success'=>1,
-                    ];
-                }
+                    }else{
 
-            }else{
+                        $deviceExists = Devices::where('expo_token',$requestDevice['expo_token'])->first();
+                        Log::info('token si existe en la base da tos ' . $deviceExists);
+                        //dd($deviceExists);
+                        $deviceExists->expo_token = $requestDevice['expo_token'];
+                        $deviceExists->save();
+                        Log::info('useeeId'. $requestDevice['userId']);
+                        $userTech = ValidationModels::validation_Technician($requestDevice['userId']);
+                        $userId = ValidationModels::validation_user($userTech->userId);
+                        Log::info('token si existe en la base da tos ' . $userId);
+                        if($userId){
 
-                $deviceExists = Devices::where('expo_token',$requestDevice['expo_token'])->first();
-                Log::info('token si existe en la base da tos ' . $deviceExists);
-                //dd($deviceExists);
-                $deviceExists->expo_token = $requestDevice['expo_token'];
-                $deviceExists->save();
-                Log::info('useeeId'. $requestDevice['userId']);
-                $userId = ValidationModels::validation_user($requestDevice['userId']);
-                Log::info('token si existe en la base da tos ' . $userId);
-                if($userId){
-
-                    $deviceUser = new DevicesUser();
-                        $deviceUser->users_id = $userId->id;
-                        $deviceUser->device_id = $deviceExists->id;
-                    $deviceUser->save();
-                    Log::info('token si existe en la base da tos ' . $deviceUser);
-                    if(!$deviceUser){
-                        DB::rollBack();
-                        return [
-                            'message' => 'Se produjo un error en el guardar dispositivo y usuario.',
-                            'success' => 2
-                        ];
+                            $deviceUser = new DevicesUser();
+                                $deviceUser->users_id = $userId->id;
+                                $deviceUser->device_id = $deviceExists->id;
+                            $deviceUser->save();
+                            Log::info('token si existe en la base da tos ' . $deviceUser);
+                            if(!$deviceUser){
+                                DB::rollBack();
+                                return [
+                                    'message' => 'Se produjo un error en el guardar dispositivo y usuario.',
+                                    'success' => 2
+                                ];
+                            }
+                            DB::commit();
+                            return [
+                                'message'=>'Actualizacion exitoso del equipo en el servidor.',
+                                'token'=> $deviceExists->expo_token,
+                                'success'=>1,
+                            ];
+                        }
                     }
-                    DB::commit();
+                } catch (\Exception $e){
+                    DB::rollBack();
                     return [
-                        'message'=>'Actualizacion exitoso del equipo en el servidor.',
-                        'token'=> $deviceExists->expo_token,
-                        'success'=>1,
+                        'message' => 'El error es el siguiente ' . $e->getMessage(),
+                        'success'=>3
                     ];
                 }
-            }
-        } catch (\Exception $e){
-            DB::rollBack();
-            return [
-                'message' => 'El error es el siguiente ' . $e->getMessage(),
-                'success'=>3
-            ];
+                break;
+
+            case "2":
+                DB::beginTransaction();
+                try {
+                    $requestDevice = $args['deviceTokenRequest'];
+                    if(!Devices::where('expo_token',$requestDevice['expo_token'])->exists()){
+                        $device = new Devices();
+                            $device->type_device = $requestDevice['type_device'];
+                            $device->name_device = $requestDevice['name_device'];
+                            $device->expo_token = $requestDevice['expo_token'];
+                        $device->save();
+
+                        //$userId = ValidationModels::validation_user($requestDevice['userId']);
+                        $userTech = ValidationModels::validation_clientInternal($requestDevice['userId']);
+                        $userId = ValidationModels::validation_user($userTech->userId);
+                        if(!$userId){
+                            $deviceUser =new DevicesUser();
+                                $deviceUser->device_id = $device->id;
+                                $deviceUser->users_id = null;
+                            $deviceUser->save();
+
+                            DB::commit();
+                            return [
+                                'message'=>'Registro exitoso del equipo invitado en el servidor.',
+                                'token'=> $device->expo_token,
+                                'success'=>1,
+                            ];
+                        }else{
+                            $deviceUser =new DevicesUser();
+                                $deviceUser->device_id = $device->id;
+                                $deviceUser->users_id = $userId->id;
+                            $deviceUser->save();
+                            DB::commit();
+                            return [
+                                'message'=>'Registro exitoso del equipo en el servidor.',
+                                'token'=> $device->expo_token,
+                                'success'=>1,
+                            ];
+                        }
+
+                    }else{
+
+                        $deviceExists = Devices::where('expo_token',$requestDevice['expo_token'])->first();
+                        Log::info('token si existe en la base da tos ' . $deviceExists);
+                        //dd($deviceExists);
+                        $deviceExists->expo_token = $requestDevice['expo_token'];
+                        $deviceExists->save();
+                        Log::info('useeeId'. $requestDevice['userId']);
+                        $userTech = ValidationModels::validation_clientInternal($requestDevice['userId']);
+                        $userId = ValidationModels::validation_user($userTech->userId);
+                        Log::info('token si existe en la base da tos ' . $userId);
+                        if($userId){
+
+                            $deviceUser = new DevicesUser();
+                                $deviceUser->users_id = $userId->id;
+                                $deviceUser->device_id = $deviceExists->id;
+                            $deviceUser->save();
+                            Log::info('token si existe en la base da tos ' . $deviceUser);
+                            if(!$deviceUser){
+                                DB::rollBack();
+                                return [
+                                    'message' => 'Se produjo un error en el guardar dispositivo y usuario.',
+                                    'success' => 2
+                                ];
+                            }
+                            DB::commit();
+                            return [
+                                'message'=>'Actualizacion exitoso del equipo en el servidor.',
+                                'token'=> $deviceExists->expo_token,
+                                'success'=>1,
+                            ];
+                        }
+                    }
+                } catch (\Exception $e){
+                    DB::rollBack();
+                    return [
+                        'message' => 'El error es el siguiente ' . $e->getMessage(),
+                        'success'=>3
+                    ];
+                }
+                break;
         }
+
+
     }
 
 }
