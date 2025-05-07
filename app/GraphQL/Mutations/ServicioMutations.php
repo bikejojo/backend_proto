8ba3098b794962b78150e351b49bafffac7e2d21
MichaelHE-WC <dev3@workcorp.net>
2025-05-07 12:57:13 -0400

    aaa

--- app/GraphQL/Mutations/ServicioMutations.php
+++ app/GraphQL/Mutations/ServicioMutations.php
@@ -408,7 +408,8 @@ class ServicioMutations
                 }
                 StatusAssigner::assignStatService($service,$this->now,self::$entity_type,'Se cancelo el servicio y la solicitud',5);
                 $service->stateId = 6;
-                if($service->typeClient === 2){
+                if($service->typeClient === "2"){
+                    dd();
                     $service->status = 1;
                     $service->save();
                     return[
