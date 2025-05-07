75dbe1efb0bbaa793d18942b18266218be35c968
MichaelHE-WC <dev3@workcorp.net>
2025-05-07 11:05:59 -0400

    notificaiotn

--- app/Listeners/NotificarSolicitudAceptada.php
+++ app/Listeners/NotificarSolicitudAceptada.php
@@ -58,7 +58,7 @@ class NotificarSolicitudAceptada
             'referencia_ubicacion' => $service->serviceLocation,
             'estado_del_servicio' => $service->stateId,
             'id_request' => $service->requestsId,
-            'description' => $service->requestDescription,
+            'description' => $service->serviceDescription,
             'date_service' => $service->updatedDateTime,
         ];
         $fecha = Carbon::parse($service->updatedDateTime);
