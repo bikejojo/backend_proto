4815795bfdf84203cbd673dedac3095945ac26e7
MichaelHE-WC <dev3@workcorp.net>
2025-05-14 11:08:52 -0400

    actividades

--- app/GraphQL/Mutations/AgendaMutations.php
+++ app/GraphQL/Mutations/AgendaMutations.php
@@ -7,6 +7,8 @@ use App\Models\Note;
 use Carbon\Carbon;
 use App\Models\Tipo_Actividad;
 Use App\Models\Tipo_Estado;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AgendaMutations
 {
@@ -15,4 +17,25 @@ class AgendaMutations
         $agendaData = $args['agendaRequest'];
     }
 
+    public function aggActivity($root, array $args){
+        DB::beginTransaction();
+        try {
+            $requestActivity  = $args['requestActivite'];
+            $activity = new Tipo_Actividad();
+                $activity->description = $requestActivity['description'];
+                $activity->entity_type = $requestActivity['entity_type'];
+            $activity->save();
+            DB::commit();
+            return [
+                'message'=>'agregado existoso',
+                'activity'=>$activity,
+            ];
+        } catch (\Exception $e) {
+            DB::rollback();
+            Log::warning('Se presentaron los siguientes problemas: ',$e->getMessage());
+            return [
+                'message' => 'problemaas de backend' . $e->getMessage()
+            ];
+        }
+    }
 }
