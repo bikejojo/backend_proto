a4bfc59f65be5e4e010c3b1d553dcaee2391db4c
MichaelHE-WC <dev3@workcorp.net>
2025-05-23 14:54:17 -0400

    url

--- config/app.php
+++ config/app.php
@@ -26,8 +26,8 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
-
+    //'env' => env('APP_ENV', 'production'),
+    'env' => env('APP_ENV', 'https://ws.alguiensabe.lat'),
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://ws.alguiensabe.lat'),//'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
