5a72a4e9b0adbffa4a97eea625cf5a5d9a8129bf
MichaelHE-WC <dev3@workcorp.net>
2025-05-23 16:01:14 -0400

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,12 +19,12 @@ return [
     'paths' => ['api/*','login', '/graphql', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        //'http://localhost:3000',
+        'http://localhost:3000',
         'http://192.168.100.17:3100',
         //'http://192.168.100.17:3000',
         'http://192.168.100.17:8020',
         'https://ws.alguiensabe.lat',
-        'http://192.168.100.216:3000/',
+        'http://192.168.100.216:3000',
         'https://panel.alguiensabe.lat'
     ],
     'allowed_origins_patterns' => ['.*'],
